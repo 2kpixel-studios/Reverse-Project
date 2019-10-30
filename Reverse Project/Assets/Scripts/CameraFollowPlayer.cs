0d394e573fbf0c437856ec78a2a021d0a1204fe2
Natalia DaLomba <nataliadalomba@gmail.com>
2019-10-30 19:33:57 -0400

    First functionality

---
+++ Reverse Project/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset = new Vector3(0.6f, 1.35f, -4.3f);
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+		transform.position = player.transform.position + offset;
+    }
+}
---
+++ Reverse Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	private float verticalInput;
+	private float horizontalInput;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+		verticalInput = Input.GetAxis("Vertical");
+		horizontalInput = Input.GetAxis("Horizontal");
+
+		//move the player forward and backwards
+		transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+		//move the player left and right
+		transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
