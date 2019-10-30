30b26fc9e413bdc283c700bdedddbec06b3b2ae4
Natalia DaLomba <nataliadalomba@gmail.com>
2019-10-30 19:57:33 -0400

    Mid way through player/camera rotating

--- Reverse Project/Assets/Scripts/PlayerMovement.cs
+++ Reverse Project/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed;
+	public float rotateSpeed;
 	private float verticalInput;
 	private float horizontalInput;
+	private float rotateInput;
     // Start is called before the first frame update
     void Start() {
         
@@ -16,11 +18,15 @@ public class PlayerMovement : MonoBehaviour {
     void Update() {
 		verticalInput = Input.GetAxis("Vertical");
 		horizontalInput = Input.GetAxis("Horizontal");
+		rotateInput = Input.GetAxis("Mouse X");
 
 		//move the player forward and backwards
 		transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
 		//move the player left and right
 		transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+		//rotate the player around the y axis
+		transform.Rotate(Vector3.up * rotateInput * Time.deltaTime * rotateSpeed);
     }
 }
